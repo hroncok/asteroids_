5eb38828ada6ee93075c82c302707d9f82515f35
Miro Hrončok <miro@hroncok.cz>
2017-12-05 20:08:06 +0100

    Copy paste the GL tick from naucse

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import math
 
 import pyglet
+from pyglet import gl
 from pyglet.window import key
 
 # constants:
@@ -74,7 +75,18 @@ def tick_all_objects(dt):
 def draw_all_objects():
     """Draws our batch"""
     window.clear()
-    batch.draw()
+    for x_offset in (-window.width, 0, window.width):
+        for y_offset in (-window.height, 0, window.height):
+            # Remember the current state
+            gl.glPushMatrix()
+            # Move everything drawn from now on by (x_offset, y_offset, 0)
+            gl.glTranslatef(x_offset, y_offset, 0)
+
+            # Draw
+            batch.draw()
+
+            # Restore remembered state (this cancels the glTranslatef)
+            gl.glPopMatrix()
 
 
 def key_pressed(sym, mod):
